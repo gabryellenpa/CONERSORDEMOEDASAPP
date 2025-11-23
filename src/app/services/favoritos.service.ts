87f1eadd45860cf9e35c26ec06f79506d399b441
Clara Gabryellen <gabryellenpaixao@gmail.com>
2025-11-23 19:40:24 -0300

    update
    c backend em nuvem

--- src/app/services/favoritos.service.ts
+++ src/app/services/favoritos.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FavoritosService {
-  private apiUrl = 'http://localhost:3000/favoritos'; 
+  private apiUrl = 'https://backend-eubs.onrender.com/'; 
 
   constructor(private http: HttpClient) {}
 
